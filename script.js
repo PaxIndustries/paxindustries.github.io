b7906ad4705de8a036680c63fa1e3cc50d6a75c2
PaxIndustries <paxindustriesofmi@gmail.com>
2025-07-08 16:40:56 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const phrases = ["Precision.", "Accountability.", "Excellence."];
-let current = 0;
-const tag = document.getElementById("rotating-tag");
-
-setInterval(() => {
-    current = (current + 1) % phrases.length;
-    tag.textContent = phrases[current];
-}, 2000);
